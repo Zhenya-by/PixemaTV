d221a7c77e96d5c27a6f888e6ea8a85935406a22
Zhenya-by <PylotEVA02@yandex.ru>
2023-07-07 13:33:18 +0300

    delite style, add Filters svg

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,3 +3,4 @@ export { ReactComponent as Home } from '../icons/Home.svg';
 export { ReactComponent as Trends } from '../icons/Trends.svg';
 export { ReactComponent as Favorites } from '../icons/Favorites.svg';
 export { ReactComponent as Setting } from '../icons/Setting.svg';
+export { ReactComponent as Filters } from '../icons/Filters.svg';
