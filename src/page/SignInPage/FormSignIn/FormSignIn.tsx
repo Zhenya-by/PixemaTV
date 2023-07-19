4d7cb366b401fa9b0645b51dd5e1c17a234c35ae
Zhenya-by <PylotEVA02@yandex.ru>
2023-07-19 13:16:39 +0300

    feat: react-hook-form upd: FormSignIn, FormSignUp upd: style

--- src/page/SignInPage/FormSignIn/FormSignIn.tsx
+++ src/page/SignInPage/FormSignIn/FormSignIn.tsx
@@ -11,8 +11,9 @@ import {
 import { useAppDispatch } from "hooks/redux-hooks";
 import { setUser } from "Store/userSlice";
 import { initializeApp } from "firebase/app";
+import { Controller, useForm } from "react-hook-form";
 
-interface IFormSignIn {}
+interface IFormSignIn { }
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -28,13 +29,9 @@ const auth = getAuth(); // Инициализация экземпляра ау�
 
 export const FormSignIn: FC<IFormSignIn> = () => {
   const dispatch = useAppDispatch();
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isAuth, setIsAuth] = useState(false);
-  const [error, setError] = useState<string | null>(null); // Состояние для сообщения об ошибке
-
   const navigate = useNavigate();
+  const [loginSuccess, setLoginSuccess] = useState(false);
+  const [isAuth, setIsAuth] = useState(false); // Initialize isAuth state
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -45,58 +42,56 @@ export const FormSignIn: FC<IFormSignIn> = () => {
         setIsAuth(false);
       }
     });
-
     return () => unsubscribe();
   }, [auth, navigate]);
 
-  const handleLogin = (name: string, email: string, password: string) => {
-    signInWithEmailAndPassword(auth, email, password)
-      .then(({ user }) => {
-        console.log(user);
-        dispatch(
-          setUser({
-            email: user.email,
-            id: user.uid,
-            token: user.refreshToken,
-            username: name,
-          })
-        );
+  const { 
+    handleSubmit,
+    control, 
+    formState: { errors }, 
+    setValue 
+  } = useForm<{
+    name: string;
+    email: string;
+    password: string;
+  }>();
 
-        // Установите имя пользователя с помощью updateProfile
-        updateProfile(user, {
-          displayName: name,
+  const handleLogin = async (data: { name: string; email: string; password: string }) => {
+    const { name, email, password } = data;
+    try {
+      const { user } = await signInWithEmailAndPassword(auth, email, password);
+      dispatch(
+        setUser({
+          email: user.email,
+          id: user.uid,
+          token: user.refreshToken,
+          username: name,
         })
-          .then(() => {
-            console.log("Username set:", name);
-            setEmail("");
-            setPassword("");
-            setError(null);
+      );
 
-            localStorage.setItem("name", name);
-            localStorage.setItem("email", email);
-            localStorage.setItem("password", password);
-
-            navigate("/home", { state: { message: "Sign in success" } });
-          })
-          .catch((error) => {
-            console.error("Error setting username:", error);
-          });
-      })
-      .catch((error) => {
-        setError(error.message);
+      // Установите имя пользователя с помощью updateProfile
+      await updateProfile(user, {
+        displayName: name,
       });
-  };
 
-  const handleChangeEmail = (newEmail: string) => {
-    setEmail(newEmail);
-  };
+      console.log("Username set:", name);
+      setValue("name", "");
+      setValue("email", "");
+      setValue("password", "");
+      setLoginSuccess(true);
 
-  const handleChangePassword = (newPassword: string) => {
-    setPassword(newPassword);
-  };
+      localStorage.setItem("name", name);
+      localStorage.setItem("email", email);
+      localStorage.setItem("password", password);
 
-  const handleChangeUsername = (newUsername: string) => {
-    setUsername(newUsername);
+      navigate("/home", { state: { message: "Sign in success" } });
+    } catch (error) {
+      if (typeof error === "string") {
+        console.error("Error registering user: " + error);
+      } else {
+        console.error("Error registering user");
+      }
+    }
   };
 
   if (isAuth) {
@@ -104,42 +99,57 @@ export const FormSignIn: FC<IFormSignIn> = () => {
     return null; // Вернуть пустой компонент, так как перенаправление уже произошло
   }
 
+  const onSubmit = handleSubmit(handleLogin);
+
   return (
-    <form
-      className="formSignIn"
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleLogin(username, email, password);
-      }}
-    >
+    <form className="formSignIn" onSubmit={onSubmit}>
       <div className="inputWraps">
         <h2 className="h2-SignIn">Sign In</h2>
-        {error && <p className="error-message">{error}</p>}
-        <Input
-          title="Username" // Добавлено поле для ввода имени пользователя
-          placeholder="Your Username"
-          value={username}
-          handleChange={handleChangeUsername}
-          isDisabled={false}
-        />
-        <Input
-          title="Email"
-          placeholder="Your Email"
-          value={email}
-          handleChange={handleChangeEmail}
-          isDisabled={false}
-        />
-        <Input
-          title="Password"
-          placeholder="Your Password"
-          value={password}
-          handleChange={handleChangePassword}
-          isDisabled={false}
-          type="password"
-        />
-        <Link className="forgot-password" to="/reset-password">
+        {loginSuccess && (
+          <p className="registration-success">Registration successful!</p>
+        )}
+        {errors.name && <p className="error-message">Username is required and must be at least 3 characters long.</p>}
+        {errors.email && <p className="error-message">Email is required.</p>}
+        {errors.password && <p className="error-message">Password is required.</p>}
+        
+        <div>
+          <label htmlFor="name">Name:</label>
+          <Controller
+            name="name"
+            control={control}
+            defaultValue=""
+            rules={{ required: true, minLength: 3 }}
+            render={({ field }) => <input className="custom-input" {...field} type="text" />}
+          />
+          {errors.name && <span>Name должен быть минимум 3 символа.</span>}
+        </div>
+
+        <div>
+          <label htmlFor="email">Email:</label>
+          <Controller
+            name="email"
+            control={control}
+            defaultValue=""
+            rules={{ required: true }}
+            render={({ field }) => <input className="custom-input" {...field} type="email" />}
+          />
+          {errors.email && <span>Email обязателен к заполнению.</span>}
+        </div>
+
+        <div>
+          <label htmlFor="password">Password:</label>
+          <Controller
+            name="password"
+            control={control}
+            defaultValue=""
+            rules={{ required: true }}
+            render={({ field }) => <input className="custom-input" {...field} type="password" />}
+          />
+          {errors.password && <span>Password обязателен к заполнению.</span>}
+        </div>
+        {/* <Link className="forgot-password" to="/reset-password">
           Forgot password?
-        </Link>
+        </Link> */}
         <div className="formBtn-Wraps">
           <button type="submit">Sign in</button>
         </div>
